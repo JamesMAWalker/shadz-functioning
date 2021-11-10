ba100d0e8d57cf3e8ed27277a5b5cf7d7cca5058
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2021-11-10 20:07:11 +0700

    Add GM verification.

--- src/components/seo.jsx
+++ src/components/seo.jsx
@@ -49,6 +49,10 @@ export function Seo({
       titleTemplate={`The Shadz | Eyewear Boutique`}
     >
       <html lang={hrefLang} />
+      <meta
+        name="google-site-verification"
+        content="DLdMyvimc3swdZB5ZGSm11fhXhczEWz8FY5VHYaduEA"
+      />
       <meta name="description" content={seo.description} />
       <meta name="image" content={seo.image} />
       <meta property="og:title" content={seo.title} />
@@ -57,18 +61,17 @@ export function Seo({
       <meta property="og:image" content={seo.image} />
       <meta property="og:type" content="website" />
       <link rel="preconnect" href="https://fonts.gstatic.com"></link>
-      <link href="https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet"></link>
+      <link
+        href="https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;1,200;1,300;1,400;1,500;1,600;1,700&display=swap"
+        rel="stylesheet"
+      ></link>
       <link
         rel="icon"
         type="image/png"
         sizes="250x250"
         href="/favicon-reg.png"
       />
-      <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href="/favicon-reg.png"
-      />
+      <link rel="apple-touch-icon" sizes="180x180" href="/favicon-reg.png" />
       {children}
     </Helmet>
   )
